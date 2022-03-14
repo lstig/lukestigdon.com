7c59d13101097411fdf1245eb6c1dbce8ad34a8d
Luke Stigdon <lstig@users.noreply.github.com>
2022-03-14 13:49:53 -0600

    cleanup NotFound page

--- src/NotFound.vue
+++ src/NotFound.vue
@@ -1,13 +1,15 @@
 <template>
   <p class="title">404</p>
-  <p class="subtitle">{{ getMsg() }}</p>
+  <p class="subtitle">
+    {{ getMsg() }}
+  </p>
 </template>
 
 <script setup lang="ts">
-import { useData } from 'vitepress'
 const msgs = [
   `Oops! Nothing to see here...`,
 ]
+
 function getMsg() {
   return msgs[Math.floor(Math.random() * msgs.length)]
 }
