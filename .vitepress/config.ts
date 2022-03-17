4d80d921c08a642a0b11d4b6f3591c62c7b5bf93
Luke Stigdon <lstig@users.noreply.github.com>
2022-03-17 13:12:11 -0600

    add favicon and remove CNAME file generation

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,6 +3,36 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "Luke Stigdon",
   description:
-    "Software Developer | DevOps Engineer | Cloud Technology Enthusiast",
+    "Software Developer, DevOps Engineer, and Cloud Technology Enthusiast.",
+  head: [
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+    ],
+    ["link", { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "favicon-32x32.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "favicon-16x16.png",
+      },
+    ],
+    ["link", { rel: "manifest", href: "/site.webmanifest" }],
+  ],
   outDir: "./dist",
 });
